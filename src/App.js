1fe784905bf9bde6e6115e6856f48c8cdd57865a
RohitSengupta22 <chints.rsg@gmail.com>
2023-11-04 12:53:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState,useEffect } from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import FetchList from './FetchList';
+import AddUser from './AddUser';
+
+
+
 
 function App() {
+
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    // Fetch users from the mock API when the component mounts
+    axios.get('https://6545e69efe036a2fa954efdb.mockapi.io/users/users')
+      .then((response) => {
+        setUsers(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching users: ', error);
+      });
+  }, []);
+
+  const addUser = (newUser) => {
+    // Add a new user
+    axios.post('https://6545e69efe036a2fa954efdb.mockapi.io/users/users', newUser)
+      .then((response) => {
+        setUsers([...users, response.data]);
+      })
+      .catch((error) => {
+        console.error('Error adding user: ', error);
+      });
+  };
+
+  const updateUser = (updatedUser) => {
+    // Update the user using their ID
+    axios.put(`https://6545e69efe036a2fa954efdb.mockapi.io/users/users/${updatedUser.id}`, updatedUser)
+      .then((response) => {
+        // Update the user in the list of users
+        const updatedUsers = users.map(user => (user.id === updatedUser.id ? updatedUser : user));
+        setUsers(updatedUsers);
+      })
+      .catch((error) => {
+        console.error('Error updating user: ', error);
+      });
+  };
+
+  const deleteUser = (userId) => {
+    axios.delete(`https://6545e69efe036a2fa954efdb.mockapi.io/users/users/${userId}`)
+      .then(() => {
+        const filteredUsers = users.filter(user => user.id !== userId);
+        setUsers(filteredUsers);
+      })
+      .catch((error) => {
+        console.error('Error deleting user: ', error);
+      });
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AddUser addUser={addUser}/>
+    <FetchList users={users} updateUser={updateUser} deleteUser={deleteUser} />
     </div>
   );
 }
